a1c333f9179e3002f36ac4fc1f59dd6faf9d39be
Sean McHugh <seanmchugh.info@gmail.com>
2020-10-05 14:44:04 -0400

    save changes before long scrape

--- ctrlfmike/TranscriptScraper/Program.cs
+++ ctrlfmike/TranscriptScraper/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System;
@@ -13,10 +14,114 @@ namespace TranscriptScraper
 {
     class Program
     {
+        static FirefoxDriver driver;
+        static string transcriptDirectory = @"C:\Users\Sean\Desktop\smchughinfo.github.io\ctrlfmike\TranscriptDirectory\";
+
         static void Main(string[] args)
         {
-            var driver = new FirefoxDriver();
-            driver.Url = "https://www.youtube.com/watch?v=ErM8ME3_HS4";
+            driver = new FirefoxDriver();
+
+            while (true)
+            {
+                try
+                {
+                    ScrapeTranscripts();
+                }
+                catch
+                {
+
+                }
+            }
+            
+            //ParseTranscripts();
+        }
+
+        static void ScrapeTranscripts()
+        {
+
+            var videoIds = GetVideosIds();
+            videoIds = videoIds.Where(id => TranscriptExists(id) == false).ToList();
+
+            foreach (var videoId in videoIds)
+            {
+                GetVideoTranscript(videoId);
+            }
+        }
+
+        static void ParseTranscripts()
+        {
+            var videoIds = Directory.GetFiles(transcriptDirectory).Select(p => Path.GetFileName(p)).ToList();
+            videoIds = videoIds.Where(v => v.EndsWith(".txt")).ToList(); // dont get the json files
+            videoIds = videoIds.Select(v => v.Replace("-timestamps.txt", "")).ToList();
+            videoIds = videoIds.Select(v => v.Replace("-transcripts.txt", "")).ToList();
+
+            foreach(var videoId in videoIds)
+            {
+                CreateJsonFile(videoId);
+            }
+        }
+
+        static string getTimestampPath(string videoId)
+        {
+            return Path.Combine(transcriptDirectory, $"{videoId}-timestamps.txt");
+        }
+
+        static string getTranscriptPath(string videoId)
+        {
+            return Path.Combine(transcriptDirectory, $"{videoId}-transcripts.txt");
+        }
+
+        static string getJsonPath(string videoId)
+        {
+            return Path.Combine(transcriptDirectory, $"{videoId}-json.json");
+        }
+
+        static void CreateJsonFile(string videoId)
+        {
+            var timestampsPath = getTimestampPath(videoId);
+            var transcriptsPath = getTranscriptPath(videoId);
+            var jsonPath = getJsonPath(videoId);
+
+            var timestamps = File.ReadAllLines(timestampsPath);
+            var transcripts = File.ReadAllLines(transcriptsPath);
+
+            JObject o = JObject.FromObject(new
+            {
+                timestamps = timestamps.ToList(),
+                transcripts = transcripts.ToList()
+            });
+
+
+            File.WriteAllText(jsonPath, o.ToString());
+        } 
+
+        static List<string> GetVideosIds()
+        {
+            driver.Url = "https://www.youtube.com/channel/UCporaXCeaOJgZKz7y3C0zbg/videos";
+
+            Thread.Sleep(10 * 1000);
+            var videoLinks = driver.FindElementsByCssSelector("ytd-grid-video-renderer a[href^='/watch?v=']");
+
+            var ids = videoLinks.Select(l => l.GetAttribute("href")).ToList();
+            ids = ids.Select(id => id.Replace("https://www.youtube.com/watch?v=", "")).ToList();
+
+            return ids;
+        }
+
+        static bool TranscriptExists(string videoId)
+        {
+            var timestampsPath = getTimestampPath(videoId);
+            var transcriptsPath = getTranscriptPath(videoId);
+
+            var timestampsExist = File.Exists(timestampsPath);
+            var transcriptsExist = File.Exists(transcriptsPath);
+
+            return timestampsExist && transcriptsExist;
+        }
+
+        static void GetVideoTranscript(string videoId)
+        {
+            driver.Url = $"https://www.youtube.com/watch?v={videoId}";
 
             Thread.Sleep(10 * 1000);
 
@@ -35,8 +140,12 @@ namespace TranscriptScraper
             var timestamps = timestampsHTML.Select(t => t.GetAttribute("textContent").Trim()).ToList();
             var transcripts = transcriptsHTML.Select(t => t.GetAttribute("textContent").Trim()).ToList();
 
-            File.WriteAllLines(@"c:\users\sean\desktop\ctrlfmike\timestamps.txt", timestamps);
-            File.WriteAllLines(@"c:\users\sean\desktop\ctrlfmike\transcripts.txt", transcripts);
+            var timestampsPath = getTimestampPath(videoId);
+            var transcriptsPath = getTranscriptPath(videoId);
+
+            File.WriteAllLines(timestampsPath, timestamps);
+            File.WriteAllLines(transcriptsPath, transcripts);
         }
+
     }
 }
